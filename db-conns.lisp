c7ed6be3fa229d79a7d67ea7b85cf317d7646b8f
Artur Gilmutdinov <arturg000@gmail.com>
2014-10-07 12:26:49 +0400

    Fixes owner-sess

---
+++ db-conns.lisp
@@ -0,0 +1,12 @@
+
+(defpackage :utils
+	(:export )
+	(:use :common-lisp))
+	
+(in-package :utils)
+
+(defun create-conn (name)
+	)
+
+(defun set-conn (conn-name)
+	)
--- owner-sess.lisp
+++ owner-sess.lisp
@@ -8,20 +8,20 @@
 
 (in-package :owner-sess)
 
-(defun create-user (login pass ownerid enabled_opts)
-	(let ((resp (utils:dbquery (format nil "select * from oshop_site.users WHERE login='~A'" login))))
+(defun create-user (sch login pass ownerid enabled_opts)
+	(let ((resp (utils:dbquery (format nil "select * from ~A.users WHERE login='~A'" sch login))))
 		(if (null resp)
 			(progn 
-				(let* ((salt (format nil "~A" (random 100000)))
+				(let* ((salt (format nil "~A" (random 100000 (make-random-state t))))
 						(passhash (utils:sha1-hash (format nil "~A~A" pass salt)))
-							(userid (car (car (utils:dbquery (format nil "INSERT INTO oshop_site.users (login, passhash, salt, ownerid, enabled_opts) VALUES ('~A','~A','~A', ~A, '~A') RETURNING id"
-							 login passhash salt ownerid (utils:to-json-string enabled_opts) )))))))
+							(userid (car (car (utils:dbquery (format nil "INSERT INTO ~A.users (login, passhash, salt, ownerid, enabled_opts) VALUES ('~A','~A','~A', ~A, '~A') RETURNING id"
+							 sch login passhash salt ownerid (utils:to-json-string enabled_opts) )))))))
 				t)
 			nil)))
 
-(defun get-user (userid)
-	(format t "get-user ~A~%" userid)
-	(let ((resp (utils:dbquery (format nil "select row_to_json (row) from oshop_site.users row WHERE id=~A" userid))))
+(defun get-user (sch userid)
+	;(format t "get-user ~A~%" userid)
+	(let ((resp (utils:dbquery (format nil "select row_to_json (row) from ~A.users row WHERE id=~A" sch userid))))
 		(if (null resp)
 			nil
 			(let ((ret (yason:parse (car (car resp)))))
@@ -29,8 +29,8 @@
 			 	ret))))
 
 ; returns userid
-(defun check-login (login pass)
-	(let ((resp (utils:dbquery (format nil "select row_to_json (row) from oshop_site.users row WHERE login='~A'" login))))
+(defun check-login (sch login pass)
+	(let ((resp (utils:dbquery (format nil "select row_to_json (row) from ~A.users row WHERE login='~A'" sch login))))
 		
 		(setf obj nil)
 		
@@ -48,25 +48,25 @@
 			nil)))
 
 ; returns (userid token)
-(defun gen-session-data (login pass)
-	(setf userid (check-login login pass))
+(defun gen-session-data (sch login pass)
+	(setf userid (check-login sch login pass))
 	(if (null userid)
 		(return-from gen-session-data nil))
-	(setf resp (utils:dbquery (format nil "select row_to_json (row) from oshop_site.sessions row WHERE userid='~A'" userid)))
+	(setf resp (utils:dbquery (format nil "select row_to_json (row) from ~A.sessions row WHERE userid='~A'" sch userid)))
 	
-	(setf token (utils:sha1-hash (format nil "~A~A" (random 10000000) (utils:unix-time) )))
+	(setf token (utils:sha1-hash (format nil "~A~A" (random 10000000 (make-random-state t)) (utils:unix-time) )))
 	
 	(if (null resp)
-		(utils:dbquery (format nil "INSERT INTO oshop_site.sessions(userid, token, creation_ts) VALUES(~A, '~A', ~A)" userid 
+		(utils:dbquery (format nil "INSERT INTO ~A.sessions(userid, token, creation_ts) VALUES(~A, '~A', ~A)" sch userid 
 				 token (utils:unix-time)))
 		
- 		(utils:dbquery (format nil "UPDATE oshop_site.sessions SET  token = '~A', creation_ts=~A WHERE userid=~A"  
- 				 token (utils:unix-time) userid)))
+ 		(utils:dbquery (format nil "UPDATE ~A.sessions SET  token = '~A', creation_ts=~A WHERE userid=~A"  
+ 				 sch token (utils:unix-time) userid)))
 	
 	(list userid token))
 
-(defun check-session (userid token)
-	(setf resp (utils:dbquery (format nil "select row_to_json (row) from oshop_site.sessions row WHERE userid='~A' AND token='~A'" userid token)))
+(defun check-session (sch userid token)
+	(setf resp (utils:dbquery (format nil "select row_to_json (row) from ~A.sessions row WHERE userid='~A' AND token='~A'" sch userid token)))
 	(if (null resp)
 		nil
 		(progn
